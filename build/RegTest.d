20467126242adb65f8841ab57b493c1987253816
putao0525 <putao0525@putao.com>
2024-06-29 18:17:06 +0800

    update

--- build/RegTest.d
+++
@@ -1,2 +0,0 @@
-/Users/putao/code/test/c/free-rtos-riscv/build/RegTest.o: \
- /Users/putao/code/test/c/free-rtos-riscv/RISC-V_RV32_QEMU_VIRT_GCC/RegTest.S
--- build/ns16550.d
+++
@@ -1,4 +0,0 @@
-/Users/putao/code/test/c/free-rtos-riscv/build/ns16550.o: \
- /Users/putao/code/test/c/free-rtos-riscv/RISC-V_RV32_QEMU_VIRT_GCC/ns16550.c \
- /Users/putao/code/test/c/free-rtos-riscv/RISC-V_RV32_QEMU_VIRT_GCC/ns16550.h
-/Users/putao/code/test/c/free-rtos-riscv/RISC-V_RV32_QEMU_VIRT_GCC/ns16550.h:
